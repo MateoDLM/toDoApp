1ea1361eb072a389ae82dea24bcffcbe542fb34f
Mateo <mateodelucamontanaro@gmail.com>
2026-03-09 14:59:15 -0300

    first commit

---
+++ todoapp/src/main/java/com/app/todoapp/model/Task.java
@@ -0,0 +1,20 @@
+package com.app.todoapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@RequiredArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private boolean completed;
+}
